19a0370efc27a558275d2f1fd062fe28f7d7a501
draquel <draquel@webjynx.com>
2018-03-16 01:58:30 -0400

    fixed editing error

--- src/config/captcha.php
+++ src/config/captcha.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'secret' => env('NOCAPTCHA_SECRET',(isset($_SERVER['NOCAPTCHA_SECRET']) ? isset($_SERVER['NOCAPTCHA_SECRET'] : NULL)),
-    'sitekey' => env('NOCAPTCHA_SITEKEY',(isset($_SERVER['NOCAPTCHA_SITEKEY']) ? isset($_SERVER['NOCAPTCHA_SITEKEY'] : NULL)),
+    'secret' => env('NOCAPTCHA_SECRET',(isset($_SERVER['NOCAPTCHA_SECRET']) ? $_SERVER['NOCAPTCHA_SECRET'] : NULL)),
+    'sitekey' => env('NOCAPTCHA_SITEKEY',(isset($_SERVER['NOCAPTCHA_SITEKEY']) ? $_SERVER['NOCAPTCHA_SITEKEY'] : NULL)),
     'options' => [
         'timeout' => 2.0,
     ],
